1e0287c363fa5dee73bb19ff1c6775c0709b17d6
Henry-Jim <henryjim1912@gmail.com>
2025-04-23 14:46:39 +0100

    Add project files.

---
+++ EventExplorer/Event.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExplorer
+{
+    public class Event
+    {
+		private string _eventID;
+
+		public string EventID
+		{
+			get { return _eventID; }
+			set { _eventID = value; }
+		}
+
+		private string _title;
+
+		public string Title
+		{
+			get { return _title; }
+			set { _title = value; }
+		}
+
+		private string _description;
+
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+
+		private DateTime _date;
+
+		public DateTime Date
+		{
+			get { return _date; }
+			set { _date = value; }
+		}
+
+		private Location _location;
+
+		public Location EventLocation
+		{
+			get { return _location; }
+			set { _location = value; }
+		}
+
+	}
+}
---
+++ EventExplorer/Location.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExplorer
+{
+    public class Location
+    {
+		private string _streetNum;
+
+		public string StreetNumber
+		{
+			get { return _streetNum; }
+			set { _streetNum = value; }
+		}
+
+		private string _streetName;
+
+		public string StreetName
+		{
+			get { return _streetName; }
+			set { _streetName = value; }
+		}
+
+		private string _city;
+
+		public string City
+		{
+			get { return _city; }
+			set { _city = value; }
+		}
+
+		private string _postalCode;
+
+		public string PostalCode
+		{
+			get { return _postalCode; }
+			set { _postalCode = value; }
+		}
+
+		private string _venueName;
+
+		public string VenueName
+		{
+			get { return _venueName; }
+			set { _venueName = value; }
+		}
+
+	}
+}
